7852e32745aff34e35ef6ce0ecb794f48ede0ea3
Tiago-Salles <tiagosalles20@gmail.com>
2022-11-30 21:24:30 -0300

    Feat: Created country details method

--- countries_app/urls.py
+++ countries_app/urls.py
@@ -3,5 +3,6 @@ from countries_app.views import *
 
 urlpatterns = [
  path('all/', get_all_countries, name='all_countries'),
+ path('<str:countryName>/', get_country_details, name="get_country_details"),
  path('create/', create_new_country, name='new_country')
 ]
